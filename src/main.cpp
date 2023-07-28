25165db24c49fe6f60c1d48431a4c80a527e7249
Baniomash <jonatase123.je@gmail.com>
2023-07-27 22:19:44 -0300

    trying to solve softwareSerial erros

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include <Arduino.h>
-#include <MPU.h>
+// #include <MPU.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-MPU accelSensor;
+// MPU accelSensor;
 TinyGPSPlus gps;
 
 void wait(unsigned long);
@@ -18,29 +17,27 @@ short int zAxis = 0;
 double lat = 0;
 double lng = 0;
 
-// short int data[5];
-
-SoftwareSerial softSerial(4, 3); // Software Serial pins (RXPin, TXPin)
+SoftwareSerial softSerial(4, 3);  // Software Serial pins (RXPin, TXPin)
 
 int timer = 0;
 boolean filesToSave = false;
 
-ISR(TIMER1_COMPA_vect){
-  if(timer >= 3){
+ISR(TIMER1_COMPA_vect) {
+  if (timer >= 3) {
     timer = 1;
-  } else{
+  } else {
     timer++;
   }
   Serial.println(timer);
 }
 
 int main() {
-  accelSensor.initialize();
-  accelSensor.disableTemperature();
-  accelSensor.disableGyroscope();
+  // accelSensor.initialize();
+  // accelSensor.disableTemperature();
+  // accelSensor.disableGyroscope();
 
-  OCR1A = 15624; //timer to interrupt = ~1sec
-  
+  OCR1A = 15624;  //timer to interrupt = ~1sec
+
   TCCR1A |= 0b00000000;
   TCCR1B |= 0b00001101;
   TIMSK1 |= 0b00000010;
@@ -50,31 +47,27 @@ int main() {
   Serial.begin(9600);
   softSerial.begin(9600);
 
-  while(1)
-	{
-    if(softSerial.available()> 0){
-      
-      if (millis() > 5000 && gps.charsProcessed() < 10){
-        Serial.println("No GPS detected: check wiring.");
-        while (true);      
-      }
-
-      if(!filesToSave && timer == 3){
-        accelSensor.readAccelerometer(&xAxis, &yAxis, &zAxis);
-        if (gps.encode(softSerial.read())){
-          lat = gps.location.lat();
-          lng = gps.location.lng();
-        }
-        getData();
-      } else if(filesToSave && timer < 3){
-        saveData();
-      }
-
-    }else{
-      Serial.println("Software Serial not working");
-      while (true);
+  while (1) {
+    if (millis() > 5000 && gps.charsProcessed() < 10) {
+      Serial.println("No GPS detected: check wiring.");
+      while (true)
+        ;
     }
-	}
+
+    if (!filesToSave && timer == 3) {
+      // accelSensor.readAccelerometer(&xAxis, &yAxis, &zAxis);
+      Serial.println("");
+      Serial.print("Dados GPS:");
+      Serial.println(softSerial.read());
+      if (gps.encode(softSerial.read())) {
+        lat = gps.location.lat();
+        lng = gps.location.lng();
+      }
+      getData();
+    } else if (filesToSave && timer < 3) {
+      saveData();
+    }
+  }
 }
 
 
@@ -89,7 +82,7 @@ int main() {
 //   }
 // }
 
-void getData(){
+void getData() {
   Serial.println("Getting data from sensors!");
   Serial.print("X ");
   Serial.println(xAxis);
@@ -104,7 +97,7 @@ void getData(){
   filesToSave = !filesToSave;
 }
 
-void saveData(){
+void saveData() {
   Serial.println("Saving data colected!");
   filesToSave = !filesToSave;
 }
