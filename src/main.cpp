d7088dce1407888472c142638241eea3cada02ad
baniomash <jonatasdez@yahoo.com.br>
2023-12-14 12:36:30 -0300

    fix: fixing some things on code

--- src/main.cpp
+++ src/main.cpp
@@ -28,78 +28,60 @@ Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
 
 // MicroSD file
-// File dataFile;
+File dataFile;
 
 void setup() {
 	Serial.begin(115200);
-  while (!Serial){
-    Serial.println("Serial problema"); //###
-	delay(10);} // will pause Zero, Leonardo, etc until serial console opens
-	// Starts Software Serial with GPS Baud Rate
-  ss.begin(GPSBaud);
-  // Initialize the microSD card
-//   if (!SD.begin(chipSelect)) {
-//     Serial.println("MicroSD initialization failed. Check your connections or card.");
-//     return;
-//   }
-  // Grants MPU is working properly
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  // MPU configs for acceleration, rotation and temperature
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+	while (!Serial){
+		Serial.println("Serial problema"); //###
+		delay(10);
+	} // will pause Zero, Leonardo, etc until serial console opens
+		// Starts Software Serial with GPS Baud Rate
+	ss.begin(GPSBaud);
+	// Initialize the microSD card
+	if (!SD.begin(chipSelect)) {
+		Serial.println("MicroSD initialization failed. Check your connections or card.");
+		return;
+	}
+	// Create a new file
+	if(SD.exists("data.csv")){
+		dataFile = SD.open("data.csv", FILE_WRITE);
+	} else {
+		dataFile = SD.open("data.csv", FILE_WRITE);
+		dataFile.print("LATITUDE,LONGITUDE,EIXO_X,EIXO_Y,EIXO_Z");
+	}
 
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+	// Grants MPU is working properly
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		while (1) {
+		delay(10);
+		}
+	}
+	// MPU configs for acceleration, rotation and temperature
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+	Serial.print("Accelerometer range set to: ");
+	switch (mpu.getAccelerometerRange()) {
+	case MPU6050_RANGE_2_G:
+		Serial.println("+-2G");
+		break;
+	case MPU6050_RANGE_4_G:
+		Serial.println("+-4G");
+		break;
+	case MPU6050_RANGE_8_G:
+		Serial.println("+-8G");
+		break;
+	case MPU6050_RANGE_16_G:
+		Serial.println("+-16G");
+		break;
+	}
 	Serial.println();
 	Serial.println(F("  Latitude   Longitude     Hour    Acceleration"));
 	Serial.println(F("    (deg)      (deg)                 X     Y      Z"));
 	Serial.println(F("-----------------------------------------------------"));
 }
 
-// This custom version of delay() ensures that the gps object
-// is being "fed".
+// This custom version of delay() ensures that the gps object is being "fed".
 static void smartDelay(unsigned long ms) {
 	unsigned long start = millis();
 	do {
@@ -113,20 +95,20 @@ static void printFloat(float val, bool valid, short int len, short int prec) {
 	if (!valid) {
 		while (len-- > 1) {
 			Serial.print('*');
-			// dataFile.print('*');
+			dataFile.print('*');
 		}
 		Serial.print(' ');
-		// dataFile.print(' ');
+		dataFile.print(',');
 	} else {
 		Serial.print(val, prec);
-		// dataFile.print(val, prec);
+		dataFile.print(val, prec);
 		int vi = abs((int)val);
 		int flen = prec + (val < 0.0 ? 2 : 1); // . and -
 		flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
 		for (int i=flen; i<len; ++i) {
 			Serial.print(' ');
-			// dataFile.print(' ');
 		}
+			dataFile.print(',');
 	}
 	smartDelay(0);
 }
@@ -148,18 +130,19 @@ static void printFloat(float val, bool valid, short int len, short int prec) {
 // 	smartDelay(0);
 // }
 
-static void printTime(TinyGPSTime &t) {
-	if (!t.isValid()) {
-		Serial.print(F("******** "));
-		// dataFile.print(F("******** "));
-	} else {
-		char sz[32];
-		sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-		Serial.print(sz);
-		// dataFile.print(sz);
-	}
-	smartDelay(0);
-}
+// static void printTime(TinyGPSTime &t) {
+// 	if (!t.isValid()) {
+// 		Serial.print(F("******** "));
+// 		// dataFile.print(F("********;"));
+// 	} else {
+// 		char sz[32];
+// 		sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+// 		Serial.print(sz);
+// 		// dataFile.print(sz);
+// 		// dataFile.print(';');
+// 	}
+// 	smartDelay(0);
+// }
 
 // static void printStr(const char *str, short int len) {
 // 	short int slen = strlen(str);
@@ -172,26 +155,22 @@ static void printTime(TinyGPSTime &t) {
 
 void loop() {
   	/* Get new sensor events with the readings */
-	mpu.getEvent(&a, &g, &temp);
-	int16_t accelX, accelY, accelZ;
-
-	// Create a new file
-  	// dataFile = SD.open("trainingVestData.txt", FILE_WRITE);
+	// mpu.getEvent(&a, &g, &temp);
 
 	// printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+	Serial.println();
+	dataFile.println();
 	printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
 	printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
 	// printInt(gps.location.age(), gps.location.isValid(), 5);
-	printTime(gps.time);
+	// printTime(gps.time);
 	// printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
 	// printInt(gps.failedChecksum(), true, 9);
-  printFloat(a.acceleration.x, true, 6, 4);
-  printFloat(a.acceleration.y, true, 6, 4);
-  printFloat(a.acceleration.z, true, 6, 4);
-	Serial.println();
-	// dataFile.println();
+	printFloat(a.acceleration.x, true, 6, 4);
+	printFloat(a.acceleration.y, true, 6, 4);
+	printFloat(a.acceleration.z, true, 6, 4);
 	smartDelay(1000);
-	// dataFile.close();
+	dataFile.flush();
 	smartDelay(2000);
 
 	if (millis() > 5000 && gps.charsProcessed() < 10) {
